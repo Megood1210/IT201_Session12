6a9dc50bead1c932cdbe9109b28ced5e36455641
Megood1210 <xuanvinhle06@gmail.com>
2025-07-07 20:42:50 +0700

    Upload bài tập 1

---
+++ PTIT_CNTT3_IT104_Session12_Bai01.c
@@ -0,0 +1,64 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+// Định nghĩa cấu trúc danh sách liên kết đôi
+typedef struct Node{
+    int data;
+    struct Node *next; // Con trỏ đến phần tử tiếp theo
+    struct Node *prev; // Con trỏ đến phần tử trước đó
+}Node;
+// Hàm tạo một Node
+Node* createNode(int value){
+    // Cấp phát bộ nhớ
+    Node *newNode = (Node*)malloc(sizeof(Node));
+    newNode->data = value; // Gán giá trị cho node
+    newNode->next = NULL;
+    newNode->prev = NULL;
+    return newNode;
+}
+// Hàm in danh sách
+void printList(Node* head){
+    Node* temp = head; // Biến trung gian
+    while(temp != NULL){
+        printf("%d <-> ",temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+    printf("\n");
+}
+// Hàm giải phóng bộ nhớ
+void freeLíst(Node* head){
+    Node* temp;
+
+    while(head != NULL){
+        temp = head;
+        head = head->next;
+        free(temp);
+    }
+}
+
+int main(){
+    Node* node1 = createNode(1);
+    Node* node2 = createNode(2);
+    Node* node3 = createNode(3);
+    Node* node4 = createNode(4);
+    Node* node5 = createNode(5);
+    // Liên kết các node với nhau
+    node1->next = node2;
+    node2->prev = node1;
+
+    node2->next = node3;
+    node3->prev = node2;
+
+    node3->next = node4;
+    node4->prev = node3;
+
+    node4->next = node5;
+    node5->prev = node4;
+
+    Node* head = node1;
+
+    printList(head);
+    freeLíst(head);
+    return 0;
+}
